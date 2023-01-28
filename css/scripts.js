9f9e2cf211287dba6381df5fa67db235a99671a8
Nathaniel Wilson <nt680476@dal.ca>
2023-01-28 13:44:03 -0400

    Add Some css changes, and some text

--- css/scripts.js
+++ css/scripts.js
@@ -1,16 +1,29 @@
- const scrollViewSideways = new IntersectionObserver((entries) => {
+ const scrollViewSidewaysLeft = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       console.log(entry)
       if(entry.isIntersecting){
-        entry.target.classList.add('show')
+        entry.target.classList.add('showLeft')
       }
       else{
-        entry.target.classList.remove('show')
+        entry.target.classList.remove('showLeft')
       }
 
     });
   });
 
+  const scrollViewSidewaysRight = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if(entry.isIntersecting){
+        entry.target.classList.add('showRight')
+      }
+      else{
+        entry.target.classList.remove('showRight')
+      }
+
+    });
+  });
+  
   const scrollViewVertical = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       console.log(entry)
@@ -23,13 +36,13 @@
 
     });
   });
-  
+
   const elemen1 = document.querySelectorAll('.skillContainer');
-  elemen1.forEach(element => scrollViewSideways.observe(element));
+  elemen1.forEach(element => scrollViewSidewaysLeft.observe(element));
   
   const elemen2 = document.querySelectorAll('.projectContainer');
-  elemen2.forEach(element => scrollViewVertical.observe(element));
+  elemen2.forEach(element => scrollViewSidewaysRight.observe(element));
 
 
   const elemen3 = document.querySelectorAll('.bio');
-  elemen3.forEach(element => scrollView.observe(element));
+  elemen3.forEach(element => scrollViewVertical.observe(element));
